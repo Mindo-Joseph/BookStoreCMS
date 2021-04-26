a8ef5a09f36050ee425837586dd5383df2bd07f4
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-04-26 22:56:31 +0300

    Add functionality to remove book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,20 @@
 import PropTypes from 'prop-types';
 
-function Book({ book: { id, title, category } }) {
+function Book({
+  book: {
+    id, title, category,
+  },
+  handleBookRemoval,
+}) {
   return (
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td>
+        <button onClick={() => handleBookRemoval({ id, title, category })} type="button">Remove</button>
+      </td>
+
     </tr>
   );
 }
@@ -15,7 +24,9 @@ Book.propTypes = {
     id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
+
   }).isRequired,
+  handleBookRemoval: PropTypes.func.isRequired,
 };
 
 export default Book;
