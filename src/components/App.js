903b8916576004308f3cc98a35c6d8750edb2c1a
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-04-28 19:03:18 +0300

    Define general styles and import them into App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Header from '../containers/Header';
+import general from '../css/general.module.css';
 
 const App = () => (
   <div>
-    <BooksList />
-    <BooksForm />
+    <Header />
+    <div className={classnames(general.flex, general.column, general.alignCenter)}>
+      <div className={general.container}>
+        <BooksList />
+        <BooksForm />
+      </div>
+    </div>
   </div>
 );
 
